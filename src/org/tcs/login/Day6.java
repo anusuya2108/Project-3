a3281c6ed25a71baa961f59ba59b5fed149ece44
anusuya22108 <anusuya2108@gmail.com>
2021-03-18 10:58:43 +0530

    project3

---
+++ src/org/tcs/login/Day6.java
@@ -0,0 +1,28 @@
+package org.tcs.login;
+
+import java.util.Base64;
+
+public class Day6 {
+	public static void main(String[] args) {
+		   String s = "GreensTechnology";
+		   String s1 = "SeleniumAutomationtool";
+		   String s2 = "velmurugan";
+		   String s4 =  "j a v a p r o g r a m";
+		   String s5 = "9095484678";
+	   int i = s.length();
+		   int j = s1.length();
+		   int k = s2.length();
+		   int l = s4.length();
+		   int m = s5.length();
+  
+
+			
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(m);
+			
+		}
+		}
+
---
+++ src/org/tcs/login/Day62.java
@@ -0,0 +1,20 @@
+package org.tcs.login;
+
+public class Day62 {
+	public static void main(String[] args) {
+		String s = "GreensTechnology";
+		int lastIndexOf = s.lastIndexOf('o');
+		System.out.println(lastIndexOf);
+		String s2 = "SeleniumAutomationtool";
+		int lastIndexOf2 = s2.lastIndexOf('n');
+		System.out.println(lastIndexOf2);
+		String s3 = "Velmurugan";
+		  int indexOf = s3.indexOf('n');
+		System.out.println(indexOf);
+		String s4 = "j a v a p r o g r a m";
+		int i = s4.lastIndexOf(' ');
+		System.out.println(i);
+			}
+	
+
+}
